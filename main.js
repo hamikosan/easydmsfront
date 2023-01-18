bb1f882f8c19d87667d90b83cccfc31bffbbd8f4
hamikosan <pimlove_c.1236@hotmail.com>
2023-01-18 22:25:26 +0700

    first commit

---
+++ main.js
@@ -0,0 +1,31 @@
+function hideIconBar(){
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:none;");
+    navigation.classList.remove("hide");
+}
+
+function showIconBar(){
+    var iconBar = document.getElementById("iconBar");
+    var navigation = document.getElementById("navigation");
+    iconBar.setAttribute("style", "display:block;");
+    navigation.classList.add("hide");
+}
+function deletePopUP(){
+    var confirmbox = document.getElementById("modal");
+    var btn = document.getElementById("del_acc");
+    var close = document.getElementById("no_button");
+    btn.onclick = function() {
+        confirmbox.style.display = "block";
+    }
+
+    close.onclick = function() {
+        confirmbox.style.display = "none";
+    }
+    
+    window.onclick = function(event) {
+        if (event.target == confirmbox) {
+            confirmbox.style.display = "none";
+        }
+    }
+}
